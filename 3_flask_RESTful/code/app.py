8082917ec1a47850e301e12968e5b086208ea147
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-11-18 11:08:39 +0100

    improved code and error control

--- 3_flask_RESTful/code/app.py
+++ 3_flask_RESTful/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -9,17 +9,29 @@ items = []
 # Every resource has to be a class
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item                # flask-restful does jsonify by itself
-        return {'item': None}, 404     # in order to get JSON, instead of null and 404
+        # returns first matching item, parameter is a default value, in case if none is found
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # flask-restful does jsonify by itself
+
+        #either matching item and 200 OK, or None and 404 
+        return {'item': item}, 200 if item is not None else 404   
 
     def post(self,name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            # 400 - Bad request
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json() # force=True don't need Content-type JSON header
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201    # 201 CREATED
 
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
 api.add_resource(Item,'/item/<string:name>')  
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000,debug=True)
